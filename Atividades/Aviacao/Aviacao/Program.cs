e6bf592a712d44a38e1032a8717d05ea2ddebc78
bianca maccarini <bianca.maccarini@gmail.com>
2024-11-05 19:54:05 -0300

    atualizar git

---
+++ Atividades/Aviacao/Aviacao/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Aviacao.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var connString = builder.Configuration["AVIACAO:ConnectionString"];
+// Fazemos a configuração do DbContext com o banco de dados específico, neste caso o SQLServer
+builder.Services.AddDbContext<AeroportoContext>(o => o.UseSqlServer(connString));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Atividades/Mercado/Mercado/Models/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+namespace Mercado.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> categorias { get; set; }
+    }
+}
